d2c91e29f28594f9f4247d21a75a14390ef8b4d5
strawboycz <mirek.precek@seznam.cz>
2022-05-26 14:00:43 +0200

    added more user interaction

--- FileManager3000/FileManager3000/Program.cs
+++ FileManager3000/FileManager3000/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Channels;
@@ -10,27 +11,30 @@ namespace FileManager3000
 		{
 				static void Main(string[] args)
 				{
+						List<string> extensions = new List<string>();
 						Console.WriteLine("FILE MANAGER 3000");
 						string directoryPath;
 						do
 						{
-							directoryPath = ReadValue("Directory", "C:\\Windows\\System32");
-							if (!Directory.Exists(directoryPath))
-							{
-								Console.WriteLine($"Directory {directoryPath} does not exist");
-							}
-						} while (!Directory.Exists(directoryPath));
-						
+								directoryPath = ReadValue("Directory", "C:\\Windows\\System32");
+								if (!Directory.Exists(directoryPath) && directoryPath == null)
+								{
+										Console.WriteLine($"Directory {directoryPath} does not exist");
+								}
+						} while (!Directory.Exists(directoryPath) && directoryPath == null);
+
+						var slectedExtensions = ReadValue("File extensions", "All extesions"); 
+
 
 						var files = Directory.GetFiles(Environment.GetFolderPath(Environment.SpecialFolder.System));
-						var fileInfos  = files.Select(x=> new FileInfo(x)).ToList();
+						var fileInfos = files.Select(x => new FileInfo(x)).ToList();
 
 						var top10Biggest = fileInfos
 							.OrderByDescending(x => x.Length)
 							.Take(10)
 							.ToList();
 						top10Biggest
-							.ForEach(x=> Console.WriteLine($"File: {x.Name},Length: {x.Length / 1024 / 1024}MB"));
+							.ForEach(x => Console.WriteLine($"File: {x.Name},Length: {x.Length / 1024 / 1024}MB"));
 						var totalSize = top10Biggest.Sum(x => x.Length);
 						var avgSize = top10Biggest.Average(x => x.Length);
 						Console.WriteLine($"Total size: {totalSize}, average size: {avgSize}");
@@ -39,7 +43,7 @@ namespace FileManager3000
 
 						var onlyPngSum = onlyPngFilenames.Select(x => new FileInfo(x)).Sum(x => x.Length);
 
-						onlyPngFilenames.ForEach(x=>Console.WriteLine(x));
+						onlyPngFilenames.ForEach(x => Console.WriteLine(x));
 
 						Console.WriteLine($"Total size of PNGs bigger than 50kB is: {onlyPngSum}");
 
